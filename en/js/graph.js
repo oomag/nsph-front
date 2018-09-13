89bb17684ffe100a8c0ac28ad32ad30d65acbe73
anton palchikov <palchikov.comunity@yandex.ru>
2018-09-13 17:33:29 +0300

    update reddit link and testnet page

---
+++ en/js/graph.js
@@ -0,0 +1,82 @@
+var ctx = document.getElementById("lineChart");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["10:50", "11:00", "11:10", "11:20", "11:30", "11:40",
+        "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50"],
+        datasets: [{
+            data: [0, 120000, 110000, 109000, 110000, 140000, 7000, 7000, 7000, 25000, 100000, 130000, 120000],
+            label: 'NZT',
+            backgroundColor: '#fda700',
+            borderColor: '#fda700',
+            borderWidth: 4,
+            fill: false
+        }]
+    },
+    options: {
+        legend: {
+            position: 'top',
+            padding: '500px'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 150000,
+                    stepSize: 50000,
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx = document.getElementById("barChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["10:50", "11:00", "11:10", "11:20", "11:30", "11:40",
+            "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50"],
+        datasets: [{
+            data: [0, 120000, 110000, 109000, 110000, 140000, 7000, 7000, 7000, 25000, 100000, 130000, 120000],
+            label: 'NZT',
+            backgroundColor: '#fda700',
+            borderColor: '#fda700',
+            borderWidth: 4,
+            fill: false
+        }]
+    },
+    options: {
+        legend: {
+            position: 'top',
+            padding: '500px'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    max: 150000,
+                    stepSize: 50000,
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx = document.getElementById("donughtChart");
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["User", "Validator"],
+        datasets: [{
+            backgroundColor: ['#fda700', '#54585a'],
+            borderColor: ["#fda700", "#54585a"],
+            data: [5000, 104],
+        }]
+    },
+    options: {
+        cutoutPercentage: 45,
+        legend: {
+            position: 'right',
+        }
+    }
+});
