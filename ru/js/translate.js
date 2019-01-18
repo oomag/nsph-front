89a2a992975b6949c11a24f5add7d0bbaad6f288
Denis Kirsanov <excite90@mail.ru>
2019-01-18 17:05:50 +0300

    added ru version

---
+++ ru/js/translate.js
@@ -0,0 +1,9 @@
+const trans = {
+    title: 'Современный промышленный блокчейн',
+    sub: 'Сервис информационной безопасности',
+    lineStart: 4,
+    lineEnd: 7,
+    secondLineStart: 1,
+    secondLineEnd: 7,
+    interval: 20
+}
